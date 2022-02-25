8ea0f6af4c281ff16f32b9b9c4a007cb82c8f145
Paulo Gouvea <paulogouvea1994@gmail.com>
2022-02-25 19:03:58 -0300

    feat: React Navigation Start

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,10 @@
-import React from "react";
-import { TouchableOpacity } from "react-native";
+import React, {
+    useEffect,
+    useState, 
+} from "react";
+import { Alert, TouchableOpacity, FlatList } from "react-native";
 import { MaterialIcons } from '@expo/vector-icons';
+import firestore from '@react-native-firebase/firestore';
 
 import happyEmoji from '../../assets/happy.png';
 
@@ -10,14 +14,58 @@ import {
     Greeting,
     GreetingEmoji,
     GreetingText,
+    Title,
+    MenuHeader,
+    MenuItemsNumber
 } from "./styles";
 
 import { useTheme } from "styled-components/native";
 
 import { Search } from "../../components/Search";
+import { ProductCard, ProductProps } from "../../components/ProductCard";
 
 export function Home(){
+    const [pizzas, setPizzas] = useState<ProductProps[]>([]);
+    const [search, setSearch] = useState('');
     const { COLORS } = useTheme();
+
+    function fetchPizzas(value: string){
+        const formattedValue = value.toLocaleLowerCase().trim();
+
+        firestore()
+        .collection('pizzas')
+        .orderBy('name_insensitive')
+        .startAt(formattedValue)
+        .endAt(`${formattedValue}\uf8ff`)
+        .get()
+        .then((response) => {
+            const data = response.docs.map(doc => {
+                return {
+                    id: doc.id,
+                    ...doc.data(),
+                }
+            }) as ProductProps[];
+
+            setPizzas(data);
+        })
+        .catch((error) => {
+            Alert.alert('Consulta', 'Não foi possível realizar a consulta');
+            console.log(error);
+        })
+    }
+
+    function handleSearch(){
+        fetchPizzas(search);
+    }
+
+    function handleSearchClear(){
+        setSearch('');
+        fetchPizzas('');
+    }
+
+    useEffect(()=> {
+        fetchPizzas('');
+    }, []);
 
     return(
         <Container>
@@ -40,9 +88,34 @@ export function Home(){
             </Header>
 
             <Search
-                onSearch={()=> {}}
-                onClear={()=> {}}
+                onChangeText={setSearch}
+                value={search}
+                onSearch={handleSearch}
+                onClear={handleSearchClear}
             />
+
+            <MenuHeader>
+                <Title>Cardápio</Title>
+                <MenuItemsNumber>10 pizzas</MenuItemsNumber>
+            </MenuHeader>
+
+            <FlatList 
+                data={pizzas}
+                keyExtractor={item => item.id}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{
+                    paddingTop: 20,
+                    paddingBottom: 125,
+                    marginHorizontal: 24
+                }}
+                renderItem={({ item }) => (
+                    <ProductCard 
+                        data={item}
+                    />
+                )}
+            />
+
+
         </Container>
     );
 }
